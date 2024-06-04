a7bfb5d6a74b33db6bdfa200e2e71bb58fbd2505
Judyannie96 <judithochuba@gmai.com>
2024-06-04 06:10:47 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,4 +1,5 @@
 #include"binary_trees.h"
+#include <stdlib.h>
 
 /**
  * binary_tree_node - creates a binary tree node
@@ -8,7 +9,7 @@
  *
  * Return: A pointer to the new node or NULL on failure
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
