c625d1017d1ac1081dcdfdcc9736c12a0985be58
Judyannie96 <judithochuba@gmai.com>
2024-06-05 12:29:14 -0700

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,7 +5,7 @@
  * binary_tree_height - Measures the height of a binary tree
  * @tree: A pointer to the root node of the tree to measure the height
  *
- * Return: The height of the binary tree.
+ * Return: 0, if tree is NULL.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
