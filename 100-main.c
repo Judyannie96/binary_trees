15cff92be6b4bd3974f6381a8ab60cc728ab16db
Judyannie96 <judithochuba@gmai.com>
2024-06-06 07:34:48 -0700

    100-binary_trees_ancestor.c

--- 100-main.c
+++ 100-main.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "binary_trees.h"
 
@@ -8,18 +7,17 @@
  * @n1: First node
  * @n2: Second node
  */
-void launch_test(binary_tree_t *n1, binary_tree_t *n2)
+void launch_test(const binary_tree_t *n1, const binary_tree_t *n2)
 {
-    binary_tree_t *ancestor;
+	const binary_tree_t *ancestor;
 
-    ancestor = binary_tree_uncle(n1, n2);
-    printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
-    if (!ancestor)
-        printf("(nil)\n");
-    else
-        printf("%d\n", ancestor->n);
+	ancestor = binary_trees_ancestor(n1, n2);
+	printf("Ancestor of [%d] & [%d]: ", n1->n, n2->n);
+	if (!ancestor)
+		printf("(nil)\n");
+	else
+		printf("%d\n", ancestor->n);
 }
-
 
 /**
  * main - Entry point
@@ -29,8 +27,7 @@ void launch_test(binary_tree_t *n1, binary_tree_t *n2)
 int main(void)
 {
 	binary_tree_t *root;
-
-       	root = binary_tree_node(NULL, 98);
+	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
 	root->left->right = binary_tree_node(root->left, 54);
